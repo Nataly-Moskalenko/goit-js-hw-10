6858fb4bb8f49b33cd6a2666ee4d09b464f223b0
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-02-14 11:17:44 +0200

    Adds Notiflix and function renderCountryInfo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-// import Notiflix from 'notiflix/build/notiflix-notify-aio';
-
-const DEBOUNCE_DELAY = 300;
+import Notiflix from 'notiflix/build/notiflix-notify-aio';
 
 const inputCountry = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+const DEBOUNCE_DELAY = 300;
 
 function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v2/name/${name}`).then(response => {
+  return fetch(
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
     if (!response.ok) {
-      throw new Error(response.status);
+      throw new Error(
+        Notiflix.Notify.failure('Oops, there is no country with that name')
+      );
     }
     return response.json();
   });
@@ -18,23 +22,46 @@ function fetchCountries(name) {
 
 function renderCountryList(countries) {
   const markup = countries
-    .map(({ name, capital, flags }) => {
+    .map(({ name, flags }) => {
       return `
       <li>                 
       <img src='${flags.svg}' width='24'></img>
-      <span>${name}</span>
-        <p>${capital}</p>
-        
+      <span>${name}</span>        
       </li>
       `;
     })
     .join('');
   countryList.innerHTML = markup;
+  countryInfo.innerHTML = '';
+}
+
+function renderCountryInfo({ name, flags, capital, population, languages }) {
+  const markup = `                       
+      <img src='${flags.svg}' width='24'></img>
+      <span>${name}</span>        
+      <p>${capital}</p>
+      <p>${population}</p>
+      <p>${languages.map(language => language.name)}</p>
+      `;
+  countryInfo.innerHTML = markup;
+  countryList.innerHTML = '';
 }
 
 function handleInput(event) {
   fetchCountries(event.target.value)
-    .then(names => renderCountryList(names))
+    .then(names => {
+      if (names.length > 10) {
+        countryInfo.innerHTML = '';
+        countryList.innerHTML = '';
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (names.length <= 10 && names.length >= 2) {
+        renderCountryList(names);
+      } else if (names.length === 1) {
+        renderCountryInfo(names[0]);
+      }
+    })
     .catch(error => console.error(error));
 }
 
